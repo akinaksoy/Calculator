b0867e1e0d3bff20a4ce9a184e0c3866bf2ff75e
Akin Aksoy <48281659+akinaksoy@users.noreply.github.com>
2022-05-23 00:42:48 +0300

    Functions Are Completed
    -Addition,Subtraction,Divide and multiply functions are completed
    -Reset function added
    -Remove last item function is added
    -getPercent function is completed

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -9,40 +9,142 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
+    var value1 : Double = 0
+    var value2 : Double = 0
+    var result : Double = 0
+    var resultText : String = "0"
+    var currentSign : String = ""
     
     @IBOutlet weak var ResultLabel: UILabel!
+    @IBOutlet weak var divideSignButton: UIButton!
+    @IBOutlet weak var multiplySignButton: UIButton!
+    @IBOutlet weak var subtractionSignButton: UIButton!
+    @IBOutlet weak var additionSignButton: UIButton!
+    @IBOutlet weak var equalSignButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func updateUI(){
+        ResultLabel.text = resultText
+        if value1 == 0 {
+          setButtonsIsSelectedFalse()
+        }
         
     }
-
-    @IBAction func numberOnClicked(_ sender: UIButton) {
+    func resultTextIsZero()-> Bool{
+        if Double(resultText)! == 0 {
+            return true
+        }else {
+            return false
+        }
     }
+ 
+    @IBAction func numberOnClicked(_ sender: UIButton) {
+        let number = sender.currentTitle!
+         if resultTextIsZero() == true || buttonsIsSelected() == true  || currentSign == "="{
+            resultText = "\(number)"
+            setButtonsIsSelectedFalse()
+        }else {
+            resultText = "\(resultText)\(number)"
+        }
+        
+        if currentSign == "=" {currentSign = ""}
+        updateUI()
+    }
+    
+    
 
     @IBAction func ResetOnClicked(_ sender: UIButton) {
+        resultText = "0"
+        value1 = 0
+        value2 = 0
+        updateUI()
     }
     
     @IBAction func deleteOnClicked(_ sender: UIButton) {
+        if resultTextIsZero() == true || resultText.count == 1{
+            resultText = "0"
+            value1 = Double(resultText)!
+        }else{
+            resultText.removeLast()
+            if resultText.last == "."{
+                resultText.removeLast()
+            }
+            value1 = Double(resultText)!
+        }
+        updateUI()
     }
     
     @IBAction func getPercentOnClicked(_ sender: UIButton) {
+        resultText = String(Double(resultText)!/1000)
+        updateUI()
     }
-    
-    
-    @IBAction func divideOnClicked(_ sender: UIButton) {
-    }
-    
-    @IBAction func multiplyOnClicked(_ sender: UIButton) {
-    }
-    
-    @IBAction func subtractionOnClicked(_ sender: UIButton) {
-    }
-    @IBAction func additionOnClicked(_ sender: UIButton) {
+
+    @IBAction func signButtonOnClicked(_ sender: UIButton) {
+        let newSign = sender.currentTitle!
+        setButtonsIsSelectedFalse()
+        
+        if newSign == "/" {
+            divideSignButton.isSelected = true
+        }else if newSign == "X"{
+            multiplySignButton.isSelected = true
+        }else if newSign == "-"{
+            subtractionSignButton.isSelected = true
+        }else if newSign == "+"{
+            additionSignButton.isSelected = true
+        }
+        
+        if value1 == 0 {
+            value1 = Double(resultText)!
+        }else {
+            calculate()
+        }
+        updateUI()
+        currentSign = newSign
     }
     
     @IBAction func equalOnClicked(_ sender: UIButton) {
+        setButtonsIsSelectedFalse()
+        calculate()
+        value1 = 0
+        currentSign = "="
+        updateUI()
+    }
+    
+    
+    func setButtonsIsSelectedFalse() {
+        additionSignButton.isSelected = false
+        subtractionSignButton.isSelected = false
+        multiplySignButton.isSelected = false
+        divideSignButton.isSelected = false
+    }
+    
+    func buttonsIsSelected()-> Bool {
+        if additionSignButton.isSelected == true || subtractionSignButton.isSelected == true || multiplySignButton.isSelected == true ||  divideSignButton.isSelected == true {
+            return true
+        }else {
+            return false
+        }
+    }
+    
+    func calculate(){
+        if currentSign == "/" {
+            value2 = Double(resultText)!
+            value1 = value1 / value2
+        }else if currentSign == "X"{
+            value2 = Double(resultText)!
+            value1 = value1 * value2
+        }else if currentSign == "-"{
+            value2 = Double(resultText)!
+            value1 = value1 - value2
+        }else if currentSign == "+"{
+            value2 = Double(resultText)!
+            value1 = value1 + value2
+        }
+        value2 = 0
+        resultText = String(value1)
     }
 }
 
