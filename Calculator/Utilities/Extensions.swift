3f12cc1c7ee15b689dddfa8880dbd8d5ecfb7685
Akin Aksoy <48281659+akinaksoy@users.noreply.github.com>
2022-05-31 23:34:37 +0300

    Extensions are Moved to Utilities

---
+++ Calculator/Utilities/Extensions.swift
@@ -0,0 +1,42 @@
+//
+//  Extensions.swift
+//  Calculator
+//
+//  Created by Akın Aksoy on 31.05.2022.
+//
+
+import Foundation
+import UIKit
+
+extension Float {
+    var CleanDecimalZero : String {
+       return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+    }
+    struct number {static var formatter = NumberFormatter()}
+    
+    var ScentificStyle: String {
+        Number.formatter.numberStyle = .scientific
+        Number.formatter.positiveFormat = "0.#########E+0"
+        Number.formatter.exponentSymbol = "e"
+        let number = NSNumber(value: self)
+        return Number.formatter.string(from :number) ?? ""
+    }
+}
+extension UILabel {
+    var NumberOfVisibleLines: Int {
+        let maxSize = CGSize(width: frame.size.width, height: CGFloat(MAXFLOAT))
+        let textHeight = sizeThatFits(maxSize).height
+        let lineHeight = font.lineHeight
+        return Int(ceil(textHeight / lineHeight))
+    }
+}
+extension UIButton {
+  func SetBackgroundColor(_ color: UIColor, forState controlState: UIControl.State) {
+    let colorImage = UIGraphicsImageRenderer(size: CGSize(width: 1, height: 1)).image { _ in
+      color.setFill()
+      UIBezierPath(rect: CGRect(x: 0, y: 0, width: 1, height: 1)).fill()
+    }
+    setTitleColor(.white, for: controlState)
+    setBackgroundImage(colorImage, for: controlState)
+  }
+}
